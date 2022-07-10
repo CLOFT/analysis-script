cf78b53923af63810c49548f0f70ff858357352d
Mateo Sheshi <sheshi.mateo01@gmail.com>
2022-07-10 23:52:47 +0200

    feat(upload-s3) : added upload report to s3

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,8 @@
 export const constants = {
   API_GATEWAY: process.env.API_GATEWAY,
   OUTPUT_FILE: process.env.OUTPUT_FILE,
+  S3_BUCKET: process.env.S3_BUCKET,
+  REGION: process.env.REGION,
+  ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
+  SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY,
 };

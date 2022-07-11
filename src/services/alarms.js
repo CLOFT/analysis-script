07f72cec99e1a07642d705aca7f9d2f967c872f2
Mateo Sheshi <sheshi.mateo01@gmail.com>
2022-07-11 11:49:59 +0200

    chore(alarms) : small change of Alarms Count endpoint

--- src/services/alarms.js
+++ src/services/alarms.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 export const countAlarms = async () => {
   let count;
   try {
-    let result = await axios.get(constants.API_GATEWAY + 'Alarms/Count');
+    let result = await axios.get(constants.API_GATEWAY + 'Alarms/LastDayCount');
     count = result.data;
   } catch (error) {
     console.log(error);
